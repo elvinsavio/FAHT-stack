96063a818e8cccd213d27ddcce1dcf4d30cba8ec
elvinsavio <elvinsavio.lp@gmail.com>
2026-02-21 21:39:22 +0000

    add accordion and typography

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
 from flask import Flask, render_template
-
+from libs import Element
+from typing import List
 
 app = Flask(__name__)
+
+
+elements: List[Element] = [
+    Element(
+        name="Typography",
+        description="A collection of typography styles and components.",
+        link="/typography",
+    ),
+    Element(
+        name="Accordion",
+        description="A simple accordion component using the <details> element.",
+        link="/accordion",
+    ),
+]
+
+
+# inject to every template
+@app.context_processor
+def inject_elements():
+    return dict(elements=elements)
 
 
 @app.route("/")
 def index():
     return render_template("landing_page.html")
 
+
+@app.route("/<path:path>")
+def catch_all(path):
+    return render_template(f"elements/{path}.html")
---
+++ libs/types.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Element:
+    name: str
+    description: str
+    link: str
