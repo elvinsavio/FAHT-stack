134eba29bb5e22b8f65dfc027899da8b7b81aad6
elvinsavio <elvinsavio.lp@gmail.com>
2026-02-25 07:45:09 +0000

    undo

--- static/js/badge.js
+++
@@ -1,8 +0,0 @@
-class XBadge extends HTMLElement {
-    connectedCallback() {
-        const variant = this.getAttribute('variant') || 'secondary';
-        this.classList.add('badge', `badge-${variant}`);
-    }
-}
-
-customElements.define('x-badge', XBadge);
